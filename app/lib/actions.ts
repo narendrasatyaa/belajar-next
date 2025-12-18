aae921cd3245db4627a280c19b8728502b7cd978
Narendra Satya Adikurniawan <narendrasatyaadi7@gmail.com>
2025-12-18 12:50:36 +0700

    feat(auth): tambah fitur login dan konfigurasi autentikasi

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -21,6 +23,25 @@ const FormSchema = z.object({
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 export async function updateInvoice(
